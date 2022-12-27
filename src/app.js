63b66c3bb02a5c44637599ca5bacf81736c6747f
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2022-12-27 16:22:49 -0500

    users Seeder

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ const authRouter = require("./auth/auth.router");
 const conversationsRouter = require("./conversations/conversations.router");
 const messagesRouter = require("./messages/messages.router");
 
+//? Seeders functions
+const createUsers = require("./utils/seeders/users");
+// const createConversations = require("./utils/seeders/conversations");
+// const createMessages = require("./utils/seeders/messages");
+// const createParitipants = require("./utils/seeders/participants");
+
 //? Initial Configs
 const app = express();
 
@@ -40,7 +46,10 @@ initModels();
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server OK!",
-    users: `localhost:${config.port}/api/v1/users`,
+    users: `https://chatapi-mc74.onrender.com/api/v1/users`,
+    conversations: `https://chatapi-mc74.onrender.com/api/v1/conversations`,
+    messages: `https://chatapi-mc74.onrender.com/api/v1/messages`,
+    participants: `https://chatapi-mc74.onrender.com/api/v1/participants`,
   });
 });
 
@@ -53,3 +62,9 @@ app.use("/api/v1/messages", messagesRouter);
 app.listen(config.port, () => {
   console.log(`Server started at port ${config.port}`);
 });
+
+//? Seeders execution
+createUsers(db);
+// createConversations(db);
+// createMessages(db);
+// createParitipants(db);
